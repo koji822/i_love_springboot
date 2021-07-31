15aa2e98c13fb1ae4b8723310950865c89875c77
yamagishikouji <yamagishikouji@localhost>
2021-07-31 19:13:42 +0900

    logライブラリをlog4jからSlf4jに変更

--- src/main/java/jp/co/commerce21/aop/AccessMonitor.java
+++ src/main/java/jp/co/commerce21/aop/AccessMonitor.java
@@ -1,71 +1,48 @@
 package jp.co.commerce21.aop;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 @Aspect
 @Component
+@Slf4j
 public class AccessMonitor {
 
 	@Before("execution(* jp.co.commerce21.controller.*.*(..))")
-	public void bfControllerLog(final JoinPoint jp) {
-		
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());		
-//		final Logger logger = Logger.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @Before " + cn + "." + mn + " ##");
+	public void bfControllerLog(final JoinPoint jp) {	
+//		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());	
+//		String cn = jp.getTarget().getClass().getSimpleName();
+//		String mn = jp.getSignature().getName();	
+//		logger.info("##  @Before " + cn + "." + mn + " ##");
+		log.info("#### メソッド開始:" + jp.getSignature());
 	}
 	
 	@After("execution(* jp.co.commerce21.controller.*.*(..))")
 	public void afControllerLog(final JoinPoint jp) {
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @After " + cn + "." + mn + " ##");
+		log.info("#### メソッド終了:" + jp.getSignature());
 	}
 	
 	@Before("execution(* jp.co.commerce21.service.*.*(..))")
 	public void bfServiceLog(final JoinPoint jp) {
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @Before " + cn + "." + mn + " ##");
+		log.info("#### メソッド開始:" + jp.getSignature());
 	}
 	
 	@After("execution(* jp.co.commerce21.service.*.*(..))")
 	public void afserviceLog(final JoinPoint jp) {
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @After " + cn + "." + mn + " ##");
+		log.info("#### メソッド終了:" + jp.getSignature());
 	}
 	
 	@Before("execution(* jp.co.commerce21.dao.*.*(..))")
 	public void bfDaoLog(final JoinPoint jp) {
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @Before " + cn + "." + mn + " ##");
+		log.info("#### メソッド開始:" + jp.getSignature());
 	}
 	
 	@After("execution(* jp.co.commerce21.dao.*.*(..))")
 	public void afDaoLog(final JoinPoint jp) {
-		final Logger logger = LogManager.getLogger(jp.getTarget().getClass());
-		
-		String cn = jp.getTarget().getClass().getSimpleName();
-		String mn = jp.getSignature().getName();	
-		logger.info("##  @After " + cn + "." + mn + " ##");
+		log.info("#### メソッド終了:" + jp.getSignature());
 	}
 }
